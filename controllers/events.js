586ee8e9aae0cc4834563c4f32f18f97d7ca6c43
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-15 19:31:12 +0100

    feat: finish create event in db

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Evento = require("../models/Evento.model");
 
 const getEvents = async (req, res = response) => {
   res.json({
@@ -8,10 +9,22 @@ const getEvents = async (req, res = response) => {
 };
 
 const createEvent = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "createEvent",
-  });
+  const evento = new Evento(req.body);
+  try {
+    evento.user = req.uid; // reference to the user who created the event
+    const createdEvent = await evento.save();
+
+    res.status(201).json({
+      ok: true,
+      evento: createdEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor, hable con el administrador",
+    });
+  }
 };
 
 const updateEvent = async (req, res = response) => {
