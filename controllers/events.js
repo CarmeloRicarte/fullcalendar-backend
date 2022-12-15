020a851ca85f89926637515bc09b66540a77a495
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-15 20:05:20 +0100

    feat: update an event

--- controllers/events.js
+++ controllers/events.js
@@ -40,10 +40,41 @@ const createEvent = async (req, res = response) => {
 };
 
 const updateEvent = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "updateEvent",
-  });
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const event = await Evento.findById(eventId);
+
+    if (!event) {
+      res.status(404).json({
+        ok: false,
+        msg: "Evento no encontrado con ese id",
+      });
+    }
+
+    // evit that another user update an event not created by this
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegios para editar este evento",
+      });
+    }
+
+    const updatedEvent = await Evento.findByIdAndUpdate(eventId, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      ok: true,
+      event: updatedEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor, hable con el administrador",
+    });
+  }
 };
 
 const deleteEvent = async (req, res = response) => {
