8db133f0c9a0e1cb11074ad804181343dfb0d355
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-15 19:50:11 +0100

    feat: get all events

--- controllers/events.js
+++ controllers/events.js
@@ -1,13 +1,25 @@
 const { response } = require("express");
 const Evento = require("../models/Evento.model");
 
+/**
+ * It gets all the events from the database and populates the user field with the name of the user
+ * @param req - The request object.
+ * @param [res] - The response object.
+ */
 const getEvents = async (req, res = response) => {
+  const events = await Evento.find().populate("user", "name");
+
   res.json({
     ok: true,
-    msg: "getEvents",
+    events,
   });
 };
 
+/**
+ * It creates a new event and saves it to the database
+ * @param req - The request object.
+ * @param [res] - The response object that will be sent back to the client.
+ */
 const createEvent = async (req, res = response) => {
   const evento = new Evento(req.body);
   try {
@@ -16,7 +28,7 @@ const createEvent = async (req, res = response) => {
 
     res.status(201).json({
       ok: true,
-      evento: createdEvent,
+      event: createdEvent,
     });
   } catch (error) {
     console.log(error);
