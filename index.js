6673c5d815c85e5c418ab33ed1d7ec36bfb64ed8
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-17 18:01:54 +0100

    fix: add path * for resolve error with routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 require("dotenv").config();
-const { dbConnection } = require("./database/config");
 const cors = require("cors");
 const app = express();
+const { dbConnection } = require("./database/config");
 
 // database setup
 dbConnection();
@@ -19,6 +19,10 @@ app.use(express.json());
 // routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(4000, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
